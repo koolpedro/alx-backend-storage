9ad471e40085daa9f70cf712d525cb47b9a4c6ee
koolpedro <panyakorah@gmail.com>
2023-07-13 03:51:26 -0700

    fix codesql script

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- script that Lists all bands with Glam as their main style, ranked by their longevity
 
 SELECT band_name, (2022 - CAST(formed AS INT) + 1) AS lifespan
-FROM bands
+FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
