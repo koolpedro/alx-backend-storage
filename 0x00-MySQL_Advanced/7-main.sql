384a26110cec42f3570f88cf4ae4fd64d5b36932
koolpedro <panyakorah@gmail.com>
2023-07-13 03:04:06 -0700

    0x00 Mysql-advanced tasks

---
+++ 0x00-MySQL_Advanced/7-main.sql
@@ -0,0 +1,28 @@
+-- Show and compute average score
+SELECT * FROM users;
+SELECT * FROM corrections;
+
+SELECT "--";
+CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
+
+SELECT "--";
+SELECT * FROM users;
+
+bob@dylan:~$ 
+bob@dylan:~$ cat 7-main.sql | mysql -uroot -p holberton 
+Enter password: 
+id  name    average_score
+1   Bob 0
+2   Jeanne  0
+user_id project_id  score
+1   1   80
+1   2   96
+2   1   91
+2   2   73
+--
+--
+--
+--
+id  name    average_score
+1   Bob 0
+2   Jeanne  82
