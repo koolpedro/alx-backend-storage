30b4ecabaf263900a752e7ea8ab1f8d42149afde
koolpedro <panyakorah@gmail.com>
2023-07-24 02:29:25 -0700

    fix code tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -28,8 +28,10 @@ def count_url_access(method):
         return html
     return wrapper
 
+
 @count_url_access
 def get_page(url: str) -> str:
     """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
+
