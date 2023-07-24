912fed3aac82dad0bf322f57ed5ca1cbf740743d
koolpedro <panyakorah@gmail.com>
2023-07-24 02:24:11 -0700

    fix code tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,33 +3,26 @@
 Web file
 """
 import requests
-import redis
-from functools import wraps
+import datetime
 
-store = redis.Redis()
-
- count_url_access(method);
-  """ This decorator keeps track of the number of times a URL is accessed """
-  @wraps(method)
-  def wrapper(url);
-    cached_key = "cached;" + url
-    cached_data = store.get(cached_key)
-    if cached_data;
-      return cached_data.decode("utf 8")
-
-    count_key = "count;" + url
-    html = method(url)
-
-    store.incr(count_key)
-    store.set(cached_key, html)
-    store.expire(cached_key, 10)
-    return html
-  return wrapper
-
-
-@count_url_access
-def get_page(url; str) > str;
-  """ Retrieves the HTML content of a given URL """
-  res = requests.get(url)
-  return res.text
-
+def get_page(url):
+    # Check if the cached result exists and has not expired
+    cacheKey = f"count:{url}"
+    cachedResult = localStorage.getItem(cacheKey)
+    cachedTimestamp = localStorage.getItem(f"{cacheKey}:timestamp")
+    currentTime = datetime.datetime.now().timestamp() * 1000
+    if cachedResult and cachedTimestamp and currentTime - cachedTimestamp <= 10000:
+        # Return the cached result
+        print(cachedResult)
+    else:
+        # Make an HTTP request to fetch the HTML content
+        response = requests.get(url)
+        if response.status_code == 200:
+            htmlContent = response.text
+            # Cache the result in localStorage
+            localStorage.setItem(cacheKey, htmlContent)
+            localStorage.setItem(f"{cacheKey}:timestamp", currentTime)
+            # Return the fetched HTML content
+            print(htmlContent)
+        else:
+            print(f"Error fetching URL: {response.status_code}")
