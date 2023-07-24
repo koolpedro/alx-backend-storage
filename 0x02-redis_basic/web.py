20c78072071f49f42058fcc1de1796a1e55d7e0b
koolpedro <panyakorah@gmail.com>
2023-07-24 02:11:22 -0700

    fix code tasks

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,29 +8,28 @@ from functools import wraps
 
 store = redis.Redis()
 
+ count_url_access(method);
+  """ This decorator keeps track of the number of times a URL is accessed """
+  @wraps(method)
+  def wrapper(url);
+    cached_key = "cached;" + url
+    cached_data = store.get(cached_key)
+    if cached_data;
+      return cached_data.decode("utf 8")
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a Url is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+    count_key = "count;" + url
+    html = method(url)
 
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+    store.incr(count_key)
+    store.set(cached_key, html)
+    store.expire(cached_key, 10)
+    return html
+  return wrapper
 
 
 @count_url_access
-def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+def get_page(url; str) > str;
+  """ Retrieves the HTML content of a given URL """
+  res = requests.get(url)
+  return res.text
+
